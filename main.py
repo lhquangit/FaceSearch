b6a8a6ae48e5ea97563751758330965c8585b6f4
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-01 13:33:27 +0700

    Create a towhee pipeline

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
+import re
 from pathlib import Path
 
 import torch
 import towhee
 
 
-DATASET = Path("datasets/rikai/")
+DATA_DIR = Path("data/rikai/")
 IS_CUDA_AVAILABLE = torch.cuda.is_available()
 DEVICE = "cuda" if IS_CUDA_AVAILABLE else "cpu"
+
+
+@towhee.register("extract_fields")
+def extract_fields(path: str) -> tuple[str, str, str, bool, bool]:
+    dir_name = Path(path).parent.name
+    fields = re.fullmatch(r"(.*) - (.*) - (.*)", dir_name)
+    company = fields.group(1)  # type: ignore
+    name = fields.group(2)  # type: ignore
+    position = fields.group(3)  # type: ignore
+    lead = "Lead" in position
+    intern = "Intern" in position
+    position = position.replace("Lead", "").replace("Intern", "").strip()
+    return company, name, position, lead, intern
 
 
 def main() -> None:
 
     dataset = (
         towhee
-        .glob(str(DATASET / "**/*"))
-        .image_decode()
-        .map(torch.from_numpy)
-        .map(lambda image: image.to(DEVICE))
-        .map(lambda image: image.permute(2, 0, 1))
-    )
+        .glob["path"](str(DATA_DIR / "**/*.png"))
+        .extract_fields["path", ("company", "name", "position", "lead", "intern")]()
+        # .image_decode["path", "image"]()
+    ) 
 
     for sample in dataset:
-        print(sample.shape, sample.device, sep="\t")
-
+        print(f"{sample.company:<15}{sample.name:<25}{sample.position:<20}{sample.lead:<5}{sample.intern:<5}")
+    
 
 if __name__ == '__main__':
     main()
---
+++ models/models.py
@@ -0,0 +1,5 @@
+import numpy as np
+from towhee._types.image import Image
+
+def embed(Image) -> np.ndarray:
+    return np.empty(512)
