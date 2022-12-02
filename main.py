7b86dd74cab111c3c0bcf6d2d082ec40ddd09ed6
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-02 08:59:02 +0700

    Fix imports

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import towhee
 
-
 DATA_DIR = Path("data/rikai/")
 
 
--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 from towhee._types.image import Image
 
+
 def embed(Image) -> np.ndarray:
     return np.empty(512)
