14648615a5dc9bec5cfb698b2d5cc5156781a90f
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-05 09:44:02 +0700

    Add packages

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ pip install pip-autoremove==0.10.0
 pip install towhee==0.8.1
 pip install opencv-python==4.6.0.66
 pip install pandas==1.5.2
+pip install protobuf==3.20.0
+pip install grpcio-tools==1.51.1
 pip install scikit-learn==1.1.3
 pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cpu
