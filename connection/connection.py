18bc72677c6efc4f43c5bc9dab3a8dcd3995fe74
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-05 13:42:31 +0700

    Add database

---
+++ connection/connection.py
@@ -0,0 +1,35 @@
+from pymilvus import (
+    DataType,
+    Collection,
+    CollectionSchema,
+    FieldSchema,
+    connections,
+    utility
+)
+
+
+def create_collection(collection_name: str, dim: int | tuple[int]):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+
+    fields = [
+        FieldSchema(
+            name='path', dtype=DataType.VARCHAR, max_length=512, is_primary=True
+        ),
+        FieldSchema(
+            name='embedding', dtype=DataType.FLOAT_VECTOR, dim=dim
+        )
+    ]
+    schema = CollectionSchema(fields=fields)
+    collection = Collection(name=collection_name, schema=schema)
+
+    index_params = {
+        'metric_type': 'L2',
+        'index_type': "IVF_FLAT",
+        'params': {"nlist": 2048},
+    }
+    collection.create_index(field_name="embedding", index_params=index_params)
+    return collection
+
+
+connections.connect(host='127.0.0.1', port='19530')
--- main.py
+++ main.py
@@ -1,43 +1,26 @@
-import re
 from pathlib import Path
-from collections import namedtuple
 
 import towhee
 
 import embedding
+from connection import create_collection
 
 DATASET = Path("data/rikai/")
 
 
-@towhee.register("extract_fields")
-def extract_fields(path: str) -> tuple[str, str, str]:
-    dir_name = Path(path).parent.name
-    match_obj = re.fullmatch(r"(.*) - (.*) - (.*)", dir_name)
-
-    field_names = ["company", "name", "position"]
-    fields = namedtuple("fields", field_names)
-
-    return fields(
-        **{
-            field_name: match_obj.group(idx)
-            for idx, field_name in enumerate(field_names, start=1)
-        }
-    )
-
-
 def main() -> None:
+    collection = create_collection("face_search", dim=512)
 
     dataset = (
         towhee
         .glob["path"](str(DATASET / "**/*.png"))
-        .extract_fields["path", ("company", "name", "position")]()
         .image_decode["path", "image"]()
         .extract_embedding["image", "embedding"]()
+        .ann_insert.milvus[("path", "embedding"), "mr"]
     )
 
     for sample in dataset:
-        print(f"{sample.company:<15}{sample.name:<25}{sample.position:<15}")
-        # print(sample.embedding.shape)
+        print(sample.embedding.shape)
 
 
 if __name__ == "__main__":
