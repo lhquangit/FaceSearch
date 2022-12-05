a1bcfcfdb548bd10ae710506951e1b39531daa3a
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-05 11:29:16 +0700

    Add database

--- database.py
+++ database.py
@@ -11,10 +11,10 @@ from pymilvus import (
 def main() -> None:
     connections.connect("default", host="localhost", port="19530")
     fields = [
-        FieldSchema(name="path", dtype=DataType.VARCHAR, is_primary=True),
-        FieldSchema(name="company", dtype=DataType.VARCHAR),
-        FieldSchema(name="name", dtype=DataType.VARCHAR),
-        FieldSchema(name="position", dtype=DataType.VARCHAR),
+        FieldSchema(name="path", dtype=DataType.VARCHAR, is_primary=True, max_length=64),
+        FieldSchema(name="company", dtype=DataType.VARCHAR, max_length=64),
+        FieldSchema(name="name", dtype=DataType.VARCHAR, max_length=64),
+        FieldSchema(name="position", dtype=DataType.VARCHAR, max_length=64),
         FieldSchema(name="image", dtype=DataType.FLOAT_VECTOR, dim=512),
     ]
     schema = CollectionSchema(fields)
