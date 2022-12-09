d0e4598c24ca9277227eeec0070c79c19e2bda8a
vuong-viet-hung <vuongviethung156@gmail.com>
2022-12-09 09:30:25 +0700

    Minor refactor

--- api.py
+++ api.py
@@ -1,31 +1,26 @@
 from io import BytesIO
 from pathlib import Path
 
-import cv2
+import numpy as np
 import towhee
 from PIL import Image
-from sklearn.metrics.pairwise import cosine_similarity
-
-
 from fastapi import FastAPI, File
 from fastapi.responses import FileResponse
 from pymilvus import Collection, connections
+from sklearn.metrics.pairwise import cosine_similarity
 
 from model import predict_face
 
 TEST_DIR = Path("test_images/")
-N_SIMILAR_IMAGES = 1
-SAME_PERSON_THRESHOLD = 0.2
+SAME_PERSON_THRESHOLD = 0.5
 
 
-def read_paths(results):
-    return [result.id for result in results]
-
-
-app = FastAPI()
+# Establish the database connection
 connections.connect(host="127.0.0.1", port="19530")
 collection = Collection("face_search")
 collection.load()
+
+app = FastAPI()
 
 
 @app.get("/")
@@ -33,38 +28,47 @@ async def root() -> dict[str, str]:
     return {"message": "Hello, World!"}
 
 
-@app.post("/similar_images")
-async def similar_images(image: bytes = File()) -> FileResponse:
+@app.post("/similar_image")
+async def similar_image(image: bytes = File()) -> FileResponse:
     pil_image = Image.open(BytesIO(image))
     pil_image.save(str(TEST_DIR / "test_image.png"))
-    results = (
+
+    data_collection = (
         towhee
         .glob["path"](str(TEST_DIR / "test_image.png"))
         .image_decode["path", "image"]()
         .extract_embedding["image", "embedding"]()
-        .ann_search.milvus["embedding", "results"](collection=collection, limit=N_SIMILAR_IMAGES)
-        .runas_op["results", "result_paths"](func=read_paths)
-        .select["image", "result_paths"]()
+        .ann_search.milvus["embedding", "result"](collection=collection)
+        .runas_op["result", "result_path"](
+            func=lambda results: [result.id for result in results]
+        )
+        .select["result_path"]()
     )
-    result_paths = [results[0].result_paths[idx] for idx in range(N_SIMILAR_IMAGES)]
-    return [FileResponse(result_path) for result_path in result_paths][0]
+
+    sample = data_collection[0]  # Grab the only sample in the data collection
+    result_path = sample.result_path
+    print(result_path)
+    return FileResponse(result_path)
 
 
 @app.post("/same_person")
-async def same_person(image_1: bytes = File(), image_2: bytes = File()) -> bool:
-    pil_image_1 = Image.open(BytesIO(image_1))
-    pil_image_1.save(str(TEST_DIR / "test_image_1.png"))
-    pil_image_2 = Image.open(BytesIO(image_2))
-    pil_image_2.save(TEST_DIR / "test_image_2.png")
-    embeddings = (
+async def same_person(images: tuple[bytes] = (File(), File())) -> bool:
+    pil_images = [
+        Image.open(BytesIO(image)) for image in images
+    ]
+    for idx, pil_image in enumerate(pil_images, start=1):
+        pil_image.save(TEST_DIR / f"test_image_{idx}.png")
+
+    data_collection = (
         towhee
         .glob["path"](TEST_DIR / "test_image_*.png")
         .image_decode["path", "image"]()
         .extract_embedding["image", "embedding"]()
     )
-    return bool(
-        cosine_similarity(
-            embeddings[0].embedding.reshape(1, -1), 
-            embeddings[1].embedding.reshape(1, -1)
-        ).squeeze() > SAME_PERSON_THRESHOLD
-    )
+
+    embeddings = [sample.embedding for sample in data_collection]
+    embeddings = [
+        np.expand_dims(embedding, axis=0) for embedding in embeddings
+    ]  # Expand the dimension of embedding for scikit-learn API compatability
+    similarity = cosine_similarity(*[embedding for embedding in embeddings]).squeeze()
+    return bool(similarity > SAME_PERSON_THRESHOLD)
